eb069a89b861ee05f5c3656c79e1bf7024d1bc5a
Sergio Ezequiel Gutierrez Alvarez <sergio@rino.lan>
2012-06-13 22:43:05 -0500

    Interfaz grafica completada
    Ahora existen todos los elementos principales

--- Core.py
+++ Core.py
@@ -18,27 +18,57 @@ class MainWindow(QMainWindow):
         
         
     def create_chart(self):
-        self.frame = QWidget()
         
+        #Generacion de elementos principales de la grafica
+        self.frame = QWidget()
         self.dpi = 100
         self.fig = Figure((6.0,5.0), dpi = self.dpi)
         self.canvas = FigureCanvas(self.fig)
         self.canvas.setParent(self.frame)
         
+        #Configuracion de la grafica
         self.axes = self.fig.add_subplot(111)
         self.axes.set_axis_bgcolor('black')
         self.axes.grid(True, color='gray')
-        vbox = QVBoxLayout()
-        vbox.addWidget(self.canvas)
-        self.pause = QPushButton('Pausa')
-        self.connect(self.pause, SIGNAL('clicked()'), self.doPause)
-        vbox.addWidget(self.pause)
+        
+        #Declaracion de interfaz grafica
+        self.pause = QPushButton('Pausar')
+        self.comenzar = QPushButton('Comenzar')
+        self.guardar = QPushButton('Guardar')
+        self.pause.clicked.connect(self.doPause)
+        self.nombreL = QLabel('Nombre de la prueba')
+        self.nombre = QLineEdit()
+        self.tiempoPrueba = QSpinBox()
+        self.tiempoPrueba.setMinimum(1)
+        self.tiempoPrueba.setDisabled(True)
+        self.politicaTiempo = QButtonGroup()
+        self.tiempoFijo = QRadioButton('Tiempo fijo')
+        self.tiempoIlimitado = QRadioButton('Tiempo ilimitado')
+        self.tiempoIlimitado.setChecked(True)
+        self.grupoTiempo = QGroupBox('Manejo de tiempo')
+        self.tiempoIlimitado.toggled.connect(self.cambiaPolitica)
+        
+        
+        hbox = QGridLayout()
+        
+        hbox.addWidget(self.tiempoIlimitado,0,0)
+        hbox.addWidget(self.tiempoFijo,1,0)
+        hbox.addWidget(self.tiempoPrueba,1,1)
+        hbox.addWidget(self.pause,0,2,2,1)
+        self.grupoTiempo.setLayout(hbox)
+        
+        vbox = QGridLayout()
+        vbox.addWidget(self.nombreL,1,0)
+        vbox.addWidget(self.nombre,1,1,)
+        vbox.addWidget(self.comenzar,1,2)
+        vbox.addWidget(self.guardar,1,3)
+        vbox.addWidget(self.grupoTiempo,2,0,1,2)
+        vbox.addWidget(self.canvas,0,0,1,4)
+        
         self.frame.setLayout(vbox)
         self.setCentralWidget(self.frame)
         self.data = []
         
-        for x in range(10):
-            self.data.append(generaData())
             
         self.line = self.axes.plot(
             self.data,
@@ -48,13 +78,24 @@ class MainWindow(QMainWindow):
            
         self.timer = QTimer()
         self.timer.timeout.connect(self.refresh)
-        self.timer.start(100)
+        self.timer.setInterval(40)
+        self.comenzar.clicked.connect(self.timer.start)
+        
+        self.draw_chart()
+        
+    def cambiaPolitica(self):
+        if self.tiempoIlimitado.isChecked():
+            self.tiempoPrueba.setDisabled(True)
+        else:
+            self.tiempoPrueba.setEnabled(True)
         
     def doPause(self):
         if self.timer.isActive():
             self.timer.stop()
+            self.pause.setText('Reanudar')
         else:
             self.timer.start()
+            self.pause.setText('Pausar')
         
     def refresh(self):
         self.data.append(generaData())
